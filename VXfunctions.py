1396ffc0a9dfbdb7da93935e98c2d7e7b18bad14
Charles <charlesgeeting@gmail.com>
2018-01-27 17:36:47 -0600

    Updated files uploaded from Dropbox.
    Initial upload was of code about a week old.  Hopefully it's important since then.

--- VXfunctions.py
+++ VXfunctions.py
@@ -1,26 +1,111 @@
-import os, sound, time
+import os, sound, time, wave, struct
+from waveGenerator import waveGenerator
+#from waveGeneratorOld import waveGeneratorOld
 
 # for waveViewTD()
 from PIL import Image
 import ImageDraw
+import ui
 
 path2_voices2emulate = './waveFiles/voices2emulate/'
 path2_transformedVoices = './waveFiles/transformedVoices/'
 path2_originalVoices = './waveFiles/originalVoices/'
 
+def getWaveFileData(waveFile):
+  """
+  # read the raw data from the wave file and return it.
+  """
+  
+  # print out wave file info.
+  print("\nwave file read:", waveFile)
+  
+  hand = wave.open(waveFile, "rb")
 
+  print("parameters:", hand.getparams()) # display wave file parameters
 
-def waveViewTD(waveFile):
-  im = Image.new("RGB", (512, 512), "white")
+  # get data all at once -10 TIMES FASTER THIS WAY!!!
+  # but...(run into memory issues this way????)
+  
+  # YOU ARE HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  bytesObj = hand.readframes(hand.getnframes()) 
+  
+  # each bytesObj unpacked must have an associated "h"
+  if hand.getnchannels() == 1:
+    fmt = str(hand.getnframes()) + "h" 
+  else:
+    fmt = str(4*hand.getnframes()) + "h"
+    
+  data = struct.unpack(fmt, bytesObj)
+
+  hand.close()  
+  
+  return data, hand.getnframes()
+
+  
+  
+  
+
+def waveViewTD(waveFile, uiImageView):
+  
+ 
+  data, numFrames = getWaveFileData(waveFile)
+ 
+  print("\nraw data read from wave file:\n")
+  print("data[0] =", data[0], "data[1] =", data[1], "data[2] =", data[2], "data[3] =", data[3], "data[4] =", data[4], "data[5] =", data[5],"...", "data[numFrames-3] =",data[numFrames-3], "data[numFrames-2] =", data[numFrames-2], "data[numFrames-1] =",data[numFrames-1])
+  
+
+  
+  
+  # now use the raw data to display the waveform(s)
+  
+  
+
+  
+
+  
+  # create image of waveform for viewing and return it.
+  """
+  im = Image.new("RGB", (1012, 596), "white")
   draw = ImageDraw.Draw(im)
   draw.line((0, 0) + im.size, fill=128)
   draw.line((0, im.size[1], im.size[0], 0), fill=128)
-  #draw.text("hello")
-  del draw 
-  
   im.save("waveFormImage.png")
-  #im.show()
+  uiImageView.image = ui.Image("waveFormImage.png")
   
+  """
+  width = 1012
+  height = 596
+  with ui.ImageContext(1012, 596) as ctx:
+    #rect = ui.Path.rounded_rect(x, y, width, height, 10)
+    oval = ui.Path.oval(width/2, height/2, 100, 100)
+    ui.set_color('red')
+    oval.fill()
+    #ui.Path.stroke()
+    img = ctx.get_image()
+ 
+  uiImageView.image = img
+  
+  
+  uiImageView.bring_to_front() # now make it visible
+
+  
+ 
+  
+   
+
+"""  
+path = path2_originalVoices
+freq = 200 # Hz
+duration = (2 + .005/4) # seconds, 1 cycle in .005 sec. So 400 and 1/4 cyles
+volume = 100 # percent
+numChan = 1 # of channels (1: mono, 2: stereo)
+
+file = waveGenerator(freq, duration, volume, numChan, path) # generate the wave file
+image = None
+waveViewTD(file, image) # now display its attributes and display it in the Time Domain
+"""
+
 
 
 def playClickSound():
@@ -110,9 +195,36 @@ def determineFileName():
   """
   returns a logical unused fileName to be used for this recording
   """
-  myTime = time.localtime()
+  myTime = time.localtime() # get local date and time
   
-  file = "recording_" + str(myTime[1]) + "_" + str(myTime[2]) + "_" + str(myTime[0]) + "_time_" + str(myTime[3]) + ":" + str(myTime[4]) + ":" + str(myTime[5]) + ".wav"
+  # prefix info. with a 0 if not a 2 digit number for sorting/display
+  if len(str(myTime[1])) == 1:
+    myTimeMonth = "0" + str(myTime[1])
+  else:
+    myTimeMonth = str(myTime[1])
+    
+  if len(str(myTime[2])) == 1:
+    myTimeDay = "0" + str(myTime[2])
+  else:
+    myTimeDay = str(myTime[2])
+    
+  if len(str(myTime[3])) == 1:
+    myTimeHours = "0" + str(myTime[3])
+  else:
+    myTimeHours = str(myTime[3])
+    
+  if len(str(myTime[4])) == 1:
+    myTimeMinutes = "0" + str(myTime[4])
+  else:
+    myTimeMinutes = str(myTime[4])
+    
+  if len(str(myTime[5])) == 1:
+    myTimeSeconds = "0" + str(myTime[5])
+  else:
+    myTimeSeconds = str(myTime[5])
+  
+  # now use date and time to create a unique file name.
+  file = "recording_" + myTimeMonth + "_" + myTimeDay + "_" + str(myTime[0]) + "_time_" + myTimeHours + ":" + myTimeMinutes + ":" + myTimeSeconds + ".wav"
   
   return file
   
--- VXgui.py
+++ VXgui.py
@@ -24,7 +24,7 @@ V. 0.0.3 Create a GUI that allows you to:
   3. Make a recording. 01/15/2018
   4. View a waveform/recording in the time domain.
   
-         view a recording, pick a "Voice" recording to emulate,
+         pick a "Voice" recording to emulate,
          create a new transformed recording, etc...
   NOTE: gui sliders to control pan and pitch probably should be added
 '''
@@ -34,7 +34,7 @@ import ui # To implement a GUI interface
 import sound # to play sound files
 import time # to shorten the length of certain sound effects
 import os # to rename a file
-
+import asyncio
 # import wave # not sure yet what its capable of.
 
 # Voice X-formation functions
@@ -42,25 +42,40 @@ from VXfunctions import voices2Emulate, originalVoices, XformedVoices, getWaveFi
 
 
 path2_originalVoices = './waveFiles/originalVoices/'
-global_buttonColor = {} # to store previous button color so that it may be reset
-# global record object so that recording doesn't stop when button function returns
+global_prevValues = {} # to store previous image values so that they may be reset
+
+# global objects so that playing and recording won't stop when button func. returns
 global_soundRecorderObj = sound.Recorder(path2_originalVoices + 'temporary.wav')
+global_soundPlayerObj = None 
 
 # GUI functions ********************************************************************
 
 # NOTE v['whatever'] are ui.View type objects!!!!!
  
-"""
+
 def textField_dataEntered(sender) :
-  # want to get rid of keyboard
-  ui.KEYBOARD_ASCII = 1
-  print(ui.KEYBOARD_DEFAULT)
-  print(ui.KEYBOARD_ASCII) # 1
-  print("text entered")
-  print(ui.KEYBOARD_NAME_PHONE_PAD)
-"""
+  # want to get rid of keyboard at this point
+  pass
+  
+   
+# not working correctly, do not call
+def restorePlayButton():
+  while True:
+    if global_soundPlayerObj.playing == False :
+      v['waveFormView'].send_to_back()
+      v['playButton'].title = "Play"
+      v['playButton'].image = global_prevValues["playButton_image"]
+      v['playButton'].background_color = global_prevValues['playButton_prevBkGrnd_color'] 
+      break
+    
+      
+  
+
  
 def playButton_tapped(sender):
+  # so sound will play when playButton_tapped function returns, if not a global it is destroyed otherwise, maybe using asyncio would get it to work too.
+  global global_soundPlayerObj
+  
   if sender.title == "Play":
     if len(v["recordingToPlay_or_Xform"].text) > 0 :
       file = getWaveFile(v["recordingToPlay_or_Xform"].text)
@@ -71,39 +86,38 @@ def playButton_tapped(sender):
         playClickSound()
         
         sender.title = "Stop Play"
-        global_buttonColor["playButton_prevBkGrnd_color"] = sender.background_color
+        global_prevValues["playButton_image"] = sender.image
+        sender.image = ui.Image("iob:stop_24")
+        
+        global_prevValues["playButton_prevBkGrnd_color"] = sender.background_color
         sender.background_color = "#ff2b62"
         v["recordingToPlay_or_Xform"].text = file.split('/')[3]
         v['recordingsView'].text = v["recordingToPlay_or_Xform"].text + " being played"
         
-        waveViewTD(file) # create an image of the waveform being played
-    
-        v['waveFormView'].image = ui.Image("waveFormImage.png")
-        v['waveFormView'].bring_to_front()
+        # create the waveform image from the sound file and display it (Time Domain)
+        # pass the wave file to read, and the imageView area to display it in
+        waveViewTD(file, v['waveFormView'])
+        print("type:", type(v['waveFormView']))   
         
+        global_soundPlayerObj = sound.Player(file)
+        global_soundPlayerObj.play()
+        #asyncio(restorePlayButton()) #does not work right
         
-        # slider comtrol for these values???
-        #pan... -1 left ear, 0 both ears, 1 right ear
-        
-        sound.play_effect(file, volume=2.0, pitch=1.0, pan=0.0, looping=False)   
-        soundPlayerObj = sound.Player(file)
-        global_buttonColor["playingNowDuration"] = soundPlayerObj.duration
-        print(global_buttonColor["playingNowDuration"])
-      
-     
+           
     else : # user has not entered a file to play yet.
       v['recordingsView'].text = "No file selected."
       sound.play_effect('game:Error')
   else: # sender.title == "Stop Play", so reset button to "Play" state
     v['waveFormView'].send_to_back()
-    
-    
-    sound.stop_all_effects() # stop the main recording being played
+      
+    if global_soundPlayerObj.playing:
+      global_soundPlayerObj.stop()
     
     playClickSound()
     
     sender.title = "Play"
-    sender.background_color = global_buttonColor["playButton_prevBkGrnd_color"]
+    sender.image = global_prevValues["playButton_image"]
+    sender.background_color = global_prevValues["playButton_prevBkGrnd_color"]
     v['recordingsView'].text = "" # clear text of what was previously playing.
     
        
@@ -114,10 +128,7 @@ def recordButton_tapped(sender):
     
     sender.title = "Stop Recording"
     
-    #v['recordButton'].image = ui.Image("iob:close_24")
-    # to use this must save prev. image to restore!
-    
-    global_buttonColor["recordButton_prevBkGrnd_color"] = sender.background_color
+    global_prevValues["recordButton_prevBkGrnd_color"] = sender.background_color
     sender.background_color = "#ff2b62"
     global_soundRecorderObj.record()
   else:
@@ -125,14 +136,12 @@ def recordButton_tapped(sender):
     playClickSound()
     
     # when recording has stopped rename the temporary file to a unique filename
-    # this function needs work, so that 1_7_2018...etc is returned as 01_07_2018
-    # so that files are sorted correctly when displayed.
     file = determineFileName() 
     path_file = path2_originalVoices + file
     os.rename('./waveFiles/originalVoices/temporary.wav', path_file)
     
     sender.title = "Record"
-    sender.background_color = global_buttonColor["recordButton_prevBkGrnd_color"]
+    sender.background_color = global_prevValues["recordButton_prevBkGrnd_color"]
  
  
  
@@ -140,11 +149,11 @@ def xButton_tapped(sender):
   playClickSound()
   if sender.title == "VX":
     sender.title = "V-Xing!"
-    global_buttonColor["xButton_prevBkGrnd_color"] = sender.background_color
+    global_prevValues["xButton_prevBkGrnd_color"] = sender.background_color
     sender.background_color = "#2bacff"
   else:
     sender.title = "VX"
-    sender.background_color = global_buttonColor["xButton_prevBkGrnd_color"]
+    sender.background_color = global_prevValues["xButton_prevBkGrnd_color"]
 
 
             
@@ -165,6 +174,7 @@ v = ui.load_view('VXgui.pyui')
 v.name = 'Voice X-former' # program title
 v.background_color = "#e0e0e0" #(0.0, 1.0, 0.0, 1.0) # (r, g, b, alpha)
 v.present('fullscreen')
+
 
 
 
